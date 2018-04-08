d47f043b2449de4b7dbb03df334b127dc04c9048
Sean Roberts <sean.roberts90@gmail.com>
2018-04-08 12:31:58 -0400

    add files

---
+++ src/integration.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const integration = (name, options) => {
+  
+};
+
+export { integration };
---
+++ src/tracking.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const track = (eventName, eventProperties) => {
+  
+}
+
+export { track };
