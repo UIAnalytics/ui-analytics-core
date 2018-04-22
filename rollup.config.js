e7a1bb0952e038522ac3fb05e65c248cb6efb51d
Sean Roberts <sean.roberts90@gmail.com>
2018-04-22 16:46:06 -0400

    adding a min configuration

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,11 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import json from 'rollup-plugin-json';
+import uglify from 'rollup-plugin-uglify';
 import { version } from './package.json';
 
-export default {
+
+export default [{
   input: 'src/index.js',
   output: {
     file: 'dist/ui-analytics.js',
@@ -23,4 +25,25 @@ export default {
   watch: {
     include: 'src/**'
   }
-};
+}, {
+  input: 'src/index.js',
+  output: {
+    file: 'dist/ui-analytics.min.js',
+    name: 'UIAnalytics',
+    format: 'iife',
+    banner: `/* ui-analytics.js v${version} by UIAnalytics.com */`
+  },
+  plugins: [
+    json(),
+    resolve(),
+    babel({
+      // only transpile our source code
+      exclude: 'node_modules/**',
+      plugins: ['external-helpers']
+    }),
+    uglify()
+  ],
+  watch: {
+    include: 'src/**'
+  }
+}];
